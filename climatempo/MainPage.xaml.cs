4dd9991bd91a3eb1c870847cef89b848fab00d2c
Renato <renato.domingues@escola.pr.gov.br>
2024-04-25 11:48:46 -0300

    25-04

--- climatempo/MainPage.xaml.cs
+++ climatempo/MainPage.xaml.cs
@@ -2,9 +2,87 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
+	Resposta resposta;
+	const string URL = "https://api.hgbrasil.com/weather?woeid&key=eeb4ae8c";
+	
+	public MainPage(){
 		InitializeComponent();
+		AtualizaTempo();
 	}
-}
 
+	async void AtualizaTempo(){
+		try{
+			var HttpClient = new HttpClient();
+			var Response = await HttpClient.GetAsync(URL);
+
+			if(Response.IsSuccessStatusCode){
+				var Content = await Response.Content.ReadAsStringAsync();
+				resposta = JsonSerializer.Deserialize<Resposta>(Content);
+			}
+		}
+
+		catch(Exception e){
+			System.Diagnostics.Debug.WriteLine(e);
+		}
+
+		PreencherTela();
+	}
+	
+
+	void PreencherTela(){
+		LabelTemperatura.Text = resposta.results.temp + "ºC".ToString();
+		LabelHumidade.Text = resposta.results.humidity.ToString();
+		
+		LabelForca.Text = resposta.results.wind_speedy.ToString();
+		LabelChuva.Text = resposta.results.rain.ToString();
+		
+		LabelTempo.Text = resposta.results.description;
+		LabelDirecao.Text = resposta.results.wind_cardinal;
+		LabelFase.Text = resposta.results.moon_phase;
+		LabelHora.Text = resposta.results.time;
+		LabelAmanhecer.Text = resposta.results.sunrise;
+		LabelAnoitecer.Text = resposta.results.sunset;
+
+		int temp = 31;
+		int sunrise = 0620;
+		int sunset = 1850;
+		int humidity = 25;
+
+		string date = "11/11/2005";
+		string time = "00:00";
+		string description = "Tempo nublado";
+		string currently = "dia";
+		string city = "Apucarana";
+		string wind_cardinal = "NE";
+		string moon_phase = "Minguante";
+
+		double cloudiness = 30;
+		double rain = 11;	
+		double wind_speedy = 5;
+
+		if(resposta.results.currently == "dia"){
+			if(resposta.results.rain > 10)
+			TelaDeFundo.Source = "dia_chuvoso.png";
+		
+			else if(resposta.results.cloudiness > 20)
+				TelaDeFundo.Source = "dia_nublado.png";
+
+			else
+				TelaDeFundo.Source = "dia_claro.png";
+		}
+		
+		else {
+			
+			if(resposta.results.rain > 10)
+			TelaDeFundo.Source = "noite_chuvosa.png";
+		
+			else if(resposta.results.cloudiness > 20)
+				TelaDeFundo.Source = "noite_nublada.png";
+
+			else
+				TelaDeFundo.Source = "noite_clara.png";
+		}
+		
+	}
+
+}
---
+++ climatempo/results.cs
@@ -0,0 +1,31 @@
+namespace climatempo;
+
+public partial class Results
+{
+
+    
+
+    public int temp {get; set;} // Temperatura
+    
+    public int humidity {get; set;} // Humidade
+    
+    public string date {get; set;} // Data
+    public string time {get; set;} // Hora
+    public string description {get; set;} // Descrição do tempo
+    public string currently {get; set;} // Dia ou Noite
+    public string city {get; set;} // Cidade
+    public string wind_cardinal {get; set;} // Direção do vento
+    public string moon_phase {get; set;} // Fase da lua
+    public string wind_speedy {get; set;} // Velocidade do vento  
+    public string sunrise {get; set;} // Amanhecer
+    public string sunset {get; set;} //Anoitecer
+
+    public double cloudiness {get; set;} // Nublado
+    public double rain {get; set;} // Chuva
+      
+
+	public Results()
+	{
+		
+	}
+}
